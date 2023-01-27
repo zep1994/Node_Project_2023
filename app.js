a16705127b69fe558629fcece9412401e118038b
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-27 10:50:46 -0600

    Add Employee

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const path = require('path')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/add-employee', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "./", 'views', 'add-employee.html'))
 })
 
 app.use((req, res, next) => {
