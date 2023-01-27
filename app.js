9e5bcd1ba39ea55a6daa6e754075978386667382
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-27 13:05:26 -0600

    Update with Employee

--- app.js
+++ app.js
@@ -1,37 +1,26 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const bodyParser = require('body-parser')
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.set("view engine", 'ejs')
-app.set('views', 'views')
+const homeData = require('./routes/home')
+const adminData = require('./routes/admin');
+const employeeRoutes = require('./routes/employee');
 
-const adminRoutes = require('./routes/admin')
-const employeeRoutes = require('./routes/employee')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/admin', adminRoutes)
-app.use(employeeRoutes)
-
-
-app.get('/', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'home.html'));
-})
-
-
+app.use(homeData)
+app.use('/admin', adminData.routes);
+app.use(employeeRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000);
