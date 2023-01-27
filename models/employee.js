a4bb85fe28269aa76037f3ac1288bb452912a35b
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2023-01-27 15:41:43 -0600

    Update employee.js

--- models/employee.js
+++ models/employee.js
@@ -1,3 +1,19 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const employeeShcema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    // description: {
+    //     type: String,
+    //     required: true
+    // }
+})
+
+
 // const employees = []
 
 // module.exports = class Employee {
